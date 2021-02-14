6c8233bb825cc71c423fa7bd53010db0c5f831ec
Nevin Herring <nevin.herring@gmail.com>
2021-02-14 16:06:48 -0600

    Logical operators and stuff

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace BooleanOperators
 {
@@ -6,7 +7,19 @@ namespace BooleanOperators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool a = true;
+            bool b = false;
+            WriteLine($"AND | a    | b ");
+            WriteLine($"a   | {a & a,-5} | {a & b,-5} ");
+            WriteLine($"b   | {b & a,-5} | {b & b,-5} ");
+            WriteLine();
+            WriteLine($"OR  | a    | b");
+            WriteLine($"a   | {a | a,-5}  | {a | b,-5} ");
+            WriteLine($"b   | {b | a,-5}  | {b | b,-5} ");
+            WriteLine();
+            WriteLine($"XOR | a     | b");
+            WriteLine($"a   | {a ^ a,-5}  | {a ^ b,-5} ");
+            WriteLine($"b   | {b ^ a,-5}  | {b ^ b,-5} ");
         }
     }
 }
